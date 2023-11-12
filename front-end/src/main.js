15a1ab0bf25034e2bd86cc42201a25044bfc40c7
Ngo Xuan Huy <ngoxuanhuy@Ngos-MacBook-Pro.local>
2023-11-12 13:50:25 +0200

    Add the front-end part

---
+++ front-end/src/main.js
@@ -0,0 +1,29 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './main.css'
+import * as VueRouter from 'vue-router'
+import ShoppingCartPage from './pages/ShoppingCartPage.vue'
+import ProductDetailPage from './pages/ProductDetailPage.vue'
+import ProductsPage from './pages/ProductsPage.vue'
+import NotFoundPage from './pages/NotFoundPage.vue'
+
+createApp(App)
+.use(VueRouter.createRouter({
+    history: VueRouter.createWebHistory(import.meta.env.BASE_URL),
+    routes:[{
+        path: '/cart',
+        component: ShoppingCartPage,
+    }, {
+        path: '/products',
+        component: ProductsPage,
+    }, {
+        path: '/products/:productId',
+        component: ProductDetailPage
+    }, {
+        path: '/:pathMatch(.*)*',
+        component: NotFoundPage
+    }]
+}))
+.mount('#app')
---
+++ front-end/src/pages/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>404: Page Note Found</h1>
+</template>
+
+<script>
+export default {
+    name: "NotFoundPage"
+}
+</script>
